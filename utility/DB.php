535284740f001032f2d36b95507b638ae2b29d4e
Marcel Glavanits <marcelglavanits@gmail.com>
2021-04-13 13:11:49 +0200

    Added basic functionality to DB class.

--- utility/DB.php
+++ utility/DB.php
@@ -1,7 +1,34 @@
 <?php
 
+include_once $_SERVER['DOCUMENT_ROOT'] . '/model/User.php';
 
 class DB
 {
+    private string $charset = 'utf8mb4';
+    private array $config;
+    private PDO $conn;
+    private array $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ];
+
+    public function __construct()
+    {
+
+        $this->config = json_decode(file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/config/config.json"),
+            true);
+        $username = $this->config["db"]["user"];
+        $password = $this->config["db"]["password"];
+        $db_name = $this->config["db"]["db_name"];
+        $dsn = "mysql:host=localhost;dbname=$db_name;charset=$this->charset";
+        try {
+
+            $this->conn = new PDO($dsn, $username, $password, $this->options);
+        } catch (Exception $e) {
+            print "Error!: " . $e->getMessage() . "<br/>";
+            die();
+        }
+    }
 
 }
