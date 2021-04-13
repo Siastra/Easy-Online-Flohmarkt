612a330b304e3668202f4eed02d7933246e32612
Marcel Glavanits <marcelglavanits@gmail.com>
2021-04-13 12:19:33 +0200

    Added user class with basic functionality.

--- model/User.php
+++ model/User.php
@@ -3,5 +3,200 @@
 
 class User
 {
+    private bool $admin;
+    private int $id, $plz;
+    private ?string $picture;
+    private string $title, $fname, $lname, $address, $password, $email, $city;
 
+    public function __construct($id, $title, $fname, $lname, $address, $plz, $city, $email, $password, $picture, $admin = 0)
+    {
+        $this->email = $email;
+        $this->fname = $fname;
+        $this->lname = $lname;
+        $this->title = $title;
+        $this->address = $address;
+        $this->city = $city;
+        $this->plz = $plz;
+        $this->admin = intval($admin);
+        $this->id = $id;
+        $this->password = $password;
+        $this->picture = $picture;
+    }
+
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isAdmin(): bool
+    {
+        return $this->admin;
+    }
+
+    /**
+     * @param int $admin
+     */
+    public function setAdmin(int $admin): void
+    {
+        $this->admin = $admin;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFname(): string
+    {
+        return $this->fname;
+    }
+
+    /**
+     * @param string $fname
+     */
+    public function setFname(string $fname): void
+    {
+        $this->fname = $fname;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLname(): string
+    {
+        return $this->lname;
+    }
+
+    /**
+     * @param string $lname
+     */
+    public function setLname(string $lname): void
+    {
+        $this->lname = $lname;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddress(): string
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param string $address
+     */
+    public function setAddress(string $address): void
+    {
+        $this->address = $address;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPlz(): int
+    {
+        return $this->plz;
+    }
+
+    /**
+     * @param int $plz
+     */
+    public function setPlz(int $plz): void
+    {
+        $this->plz = $plz;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCity(): string
+    {
+        return $this->city;
+    }
+
+    /**
+     * @param string $city
+     */
+    public function setCity(string $city): void
+    {
+        $this->city = $city;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPicture(): ?string
+    {
+        return $this->picture;
+    }
+
+    /**
+     * @param string $picture
+     */
+    public function setPicture(string $picture): void
+    {
+        $this->picture = $picture;
+    }
 }
