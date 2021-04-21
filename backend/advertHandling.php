f42258fdc8000be1afcb2ba4107fabc54ba1024b
Sebastian2123 <sebastian_schramm@hotmail.com>
2021-04-21 18:44:26 +0200

    added advert Handling

---
+++ backend/advertHandling.php
@@ -0,0 +1,14 @@
+<?php
+
+include_once $_SERVER['DOCUMENT_ROOT'] . '/Easy-Online-Flohmarkt/backend/utility/DB.php';
+
+$db = new DB();
+$advertisment = new Advert(1,1, $_REQUEST["title"], $_REQUEST["price"], $_REQUEST["description"]);
+
+    if ($db->createAdv($advertisment)) {
+        header("Location: ../index.php?section=upload");
+    }else {
+        header("Location: ../index.php?section=register");
+}
+
+?>
