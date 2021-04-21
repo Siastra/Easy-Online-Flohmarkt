0b8f796a78553f9b60ce6ce65f989e58e5c0fe2f
Sebastian2123 <sebastian_schramm@hotmail.com>
2021-04-21 18:55:39 +0200

    added new Location after successful creation

--- backend/advertHandling.php
+++ backend/advertHandling.php
@@ -6,9 +6,9 @@ $db = new DB();
 $advertisment = new Advert(1,1, $_REQUEST["title"], $_REQUEST["price"], $_REQUEST["description"]);
 
     if ($db->createAdv($advertisment)) {
-        header("Location: ../index.php?section=upload");
+        header("Location: ../index.php?section=dashboard");
     }else {
-        header("Location: ../index.php?section=register");
+        header("Location: ../index.php?section=upload");
 }
 
 ?>
